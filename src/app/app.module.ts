de3a65710901c9a035798aa546e73219e1dd0ad9
NatyNatur <naticcg@gmail.com>
2018-03-09 16:23:49 -0300

    modal de registro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 
-
 import { AppComponent } from './app.component';
 import { PoemComponent } from './poem/poem.component';
 import { VerseFormComponent } from './verse-form/verse-form.component';
@@ -12,21 +11,36 @@ import { AngularFireModule} from 'angularfire2';
 import { environment } from '../environments/environment';
 import { UserRegisterComponent } from './user-register/user-register.component';
 
+// Componentes externos
+import { MatDialogModule } from '@angular/material/dialog';
+import { RegistrationModalComponent } from './registration-modal/registration-modal.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent,
     PoemComponent,
     VerseFormComponent,
     VerseCompComponent,
-    UserRegisterComponent
+    UserRegisterComponent,
+    RegistrationModalComponent
   ],
   imports: [
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     ReactiveFormsModule,  
-    BrowserModule
+    BrowserModule,
+
+    // Componentes externos
+    MatDialogModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [
+    UserRegisterComponent,
+    RegistrationModalComponent
+
+  ]
 })
 export class AppModule { }
--- src/app/user-register/user-register.component.ts
+++ src/app/user-register/user-register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import { RegistrationModalComponent } from '../registration-modal/registration-modal.component';
 
 @Component({
   selector: 'app-user-register',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserRegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
+  openDialog():void {
+    let dialogRef = this.dialog.open(RegistrationModalComponent, {
+      width: '250px',
+      data: {}
+    });
+  // una referencia es la forma de acceder a la información ingresada en el diálogo
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+  
   ngOnInit() {
+    this.openDialog();
   }
 
 }
