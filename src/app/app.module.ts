3e21c56a98a4b63837dff81c9efc576a210858c3
NatyNatur <naticcg@gmail.com>
2018-03-09 15:09:25 -0300

    arreglando versos mnñe, agregando firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 
 import { AppComponent } from './app.component';
 import { PoemComponent } from './poem/poem.component';
 import { VerseFormComponent } from './verse-form/verse-form.component';
 import { VerseCompComponent } from './verse-comp/verse-comp.component';
-
+import { AngularFireModule} from 'angularfire2';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -17,6 +19,8 @@ import { VerseCompComponent } from './verse-comp/verse-comp.component';
     VerseCompComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
     ReactiveFormsModule,  
     BrowserModule
   ],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBSgRmU9ZzxXkC3olgWrT4-aY-EHDsAzKk",
+    authDomain: "cadaverexquisitoenangular.firebaseapp.com",
+    databaseURL: "https://cadaverexquisitoenangular.firebaseio.com",
+    projectId: "cadaverexquisitoenangular",
+    storageBucket: "cadaverexquisitoenangular.appspot.com",
+    messagingSenderId: "1051797903067"
+  }
+}
